4266b0134c363d0669c732eab69b828bd2900e16
jjjjj121 <104483496+jjjjj121@users.noreply.github.com>
2023-06-16 14:22:05 +0900

    first commit

---
+++ Source/RPGGame/RPGGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RPGGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPGGame, "RPGGame" );
+ 
---
+++ Source/RPGGame/RPGGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARPGGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARPGGameGameMode();
+};
+
+
+
